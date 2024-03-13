51147437c535a0d2c78644d3b386f43d7e91f4a8
Diamondd01 <Nadiamondj@gmail.com>
2024-03-13 15:33:57 -0400

    sales class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,5 +1,18 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
+class SalesPerson extends Employees {
+    #totalSales;
+    constructor(clients,totalSales,name,position,salary,isHired){
+        super(name,position,salary,isHired);
+        this.clients=clients;
+        this.#totalSales=0
+    }
+    getSalesNumbers(){
+        return this.#totalSales;
+    }
+    makeSale(amount){
+        return this.#totalSales= amount;
+    }
 
 
     
