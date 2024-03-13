2b093d9348aeee3e310887a1cfbb52f849f8985d
Chemeeka Bahe <bchemeeka@gmail.com>
2024-03-13 12:53:03 -0700

    update managers

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,6 +1,16 @@
-class Manager {
-
-
+const { Employees} = require("./Employees")
+class Manager extends Employees{
+        #employeesManaged = []
+constructor(department,employeesManaged){
+    this.department = department;
+    this.#employeesManaged = employeesManaged;
+}
+getEmployeeManaged(){
+    return this.#employeesManaged
+}
+setEmployeeManaged(employee){
+    return this.#employeesManaged = employee;
+}
 
     
 }
